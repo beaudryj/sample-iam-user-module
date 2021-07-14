20bfcdd215dfbc5dc2c22ed23a547091a81c85ff
jbeaudry <joe.beaudry@slalom.com>
2021-07-14 01:57:22 -0400

    issue with role reference

--- policyattach.tf
+++ policyattach.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role_policy_attachment" "user-policy-attachment" {
-  role       = aws_iam_group.user.name
+  role       = aws_iam_user.user.name
   policy_arn = var.create_policy ? aws_iam_policy.policy[0].arn : var.existing_policy_arn
 
   depends_on = [aws_iam_user.user, aws_iam_policy.policy]
