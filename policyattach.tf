3ca5350436ecc1113bc5e07c533b897a0fbfc308
jbeaudry <joe.beaudry@slalom.com>
2021-07-14 15:27:49 -0400

    used wrong attachment type

--- policyattach.tf
+++ policyattach.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "user-policy-attachment" {
+resource "aws_iam_user_policy_attachment" "user-policy-attachment" {
   role       = aws_iam_user.user.name
   policy_arn = var.create_policy ? aws_iam_policy.policy[0].arn : var.existing_policy_arn
 
